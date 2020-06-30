9777eafc2e51bbe17fb862e12376d1b996e9ebfb
richardszegh <richard.szegh@gmail.com>
2020-06-30 15:48:29 +0200

    add header

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Header from './header'
+
 export default function Layout({ children }) {
   return (
     <>
+      <Header />
+
       <div>
         <main>{children}</main>
       </div>
