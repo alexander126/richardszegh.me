f40dbb929b299446dad714477b27af86ed0725a0
richardszegh <richard.szegh@gmail.com>
2020-07-01 14:10:59 +0200

    footer done

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { PRIMARY_COLOR } from '../../style.config'
+
 const FooterContainer = styled.footer`
   display: flex;
   align-items: center;
   justify-content: center;
+  background-color: ${PRIMARY_COLOR};
+  color: #fff;
+  padding-top: 24px;
+  padding-bottom: 24px;
 `
 
 export default function Footer() {
