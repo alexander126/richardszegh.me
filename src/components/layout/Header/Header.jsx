4ab822165b5ae5bf14499d2dfc40a1a2bea44f70
richardszegh <richard.szegh@gmail.com>
2020-07-01 20:24:54 +0200

    add header logo

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 import { HEADER_HEIGHT, PRIMARY_COLOR } from '../../../style.config'
 
@@ -17,11 +19,11 @@ const HeaderContainer = styled.div`
   background-color: ${props => (props.scrolled ? '#fff' : 'transparent')};
   z-index: 9999;
   color: ${props => (props.scrolled ? PRIMARY_COLOR : '#fff')};
-  padding-top: 24px;
-  padding-bottom: 24px;
   box-shadow: ${props =>
     props.scrolled ? '0px 0px 15px 0px rgba(0, 0, 0, 0.75)' : 'none'};
   transition: 0.3s ease-in-out;
+  display: flex;
+  align-items: center;
 `
 
 const HeaderContent = styled.div`
@@ -31,11 +33,37 @@ const HeaderContent = styled.div`
 `
 
 export default function Header({ scrolled }) {
+  const data = useStaticQuery(graphql`
+    query {
+      primaryLogo: file(relativePath: { eq: "logo_primary.png" }) {
+        childImageSharp {
+          fixed(height: 50, width: 50) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+
+      secondaryLogo: file(relativePath: { eq: "logo_secondary.png" }) {
+        childImageSharp {
+          fixed(height: 50, width: 50) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <HeaderContainer scrolled={scrolled}>
       <Container>
         <HeaderContent>
-          <span>{'LOGO'}</span>
+          <Img
+            fixed={
+              scrolled
+                ? data.primaryLogo.childImageSharp.fixed
+                : data.secondaryLogo.childImageSharp.fixed
+            }
+          />
 
           <DesktopNavigation />
         </HeaderContent>
