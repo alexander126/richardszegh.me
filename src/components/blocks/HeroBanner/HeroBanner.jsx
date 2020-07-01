bbca514a751538d6e00162831d22dc3641a78081
richardszegh <richard.szegh@gmail.com>
2020-07-01 19:17:29 +0200

    hero banner #1

--- src/components/blocks/HeroBanner/HeroBanner.jsx
+++ src/components/blocks/HeroBanner/HeroBanner.jsx
@@ -1,32 +1,112 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components'
+import BackgroundImage from 'gatsby-background-image'
+
+import { HEADER_HEIGHT, PRIMARY_COLOR } from '../../../style.config'
+
+import Container from '../../layout/Container'
 
 import Button from '../../UI/Button'
 
-export default function HeroBanner() {
-  return (
-    <div
-      style={{
-        padding: 18,
-        display: 'flex',
-        justifyContent: 'space-around',
-      }}
-    >
-      <div
-        style={{
-          padding: 24,
-          backgroundColor: '#0a1147',
-        }}
-      >
-        <Button variant="primary">{'Primary button'}</Button>
-      </div>
+const HeroBannerBackground = styled(BackgroundImage)`
+  padding-top: ${HEADER_HEIGHT}px;
+  height: 100vh;
+  color: #fff;
+`
 
-      <div
-        style={{
-          padding: 24,
-        }}
-      >
-        <Button variant="secondary">{'Secondary button'}</Button>
-      </div>
-    </div>
+const HeroBannerContentWrapper = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+`
+
+const HeroBannerContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`
+
+const HeroBannerContentText = styled.div`
+  max-width: 450px;
+  text-align: right;
+`
+
+const Welcome = styled.h1`
+  line-height: 1.25;
+  margin-bottom: 42px;
+`
+
+const MissionContainer = styled.div`
+  margin-bottom: 42px;
+`
+
+const MissionQuestion = styled.h4`
+  margin-bottom: 0.75rem;
+  display: inline-block;
+  background-color: #fff;
+  color: ${PRIMARY_COLOR};
+  padding-top: 0.35rem;
+  padding-bottom: 0.35rem;
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
+`
+
+const Mission = styled.h1`
+  line-height: 1.25;
+  font-size: 3rem;
+`
+
+const ButtonContainer = styled.div`
+  text-align: center;
+`
+
+export default function HeroBanner() {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "abstract_space.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1920, quality: 100) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <HeroBannerBackground
+      Tag="section"
+      fluid={data.file.childImageSharp.fluid}
+      backgroundColor={PRIMARY_COLOR}
+    >
+      <HeroBannerContentWrapper>
+        <Container>
+          <HeroBannerContent>
+            <HeroBannerContentText>
+              <Welcome>
+                Hey, my name is Richárd,
+                <br />
+                I'm a developer consultant.
+              </Welcome>
+
+              <MissionContainer>
+                <MissionQuestion>My mission is to:</MissionQuestion>
+
+                <Mission>
+                  Materialize visions through masterfully crafted software.
+                </Mission>
+              </MissionContainer>
+
+              <ButtonContainer>
+                <Button variant="primary" size="large">
+                  {'View services'}
+                </Button>
+              </ButtonContainer>
+            </HeroBannerContentText>
+          </HeroBannerContent>
+        </Container>
+      </HeroBannerContentWrapper>
+    </HeroBannerBackground>
   )
 }
