1924275f397eaec197c0bb5f4c5c6e40dd5bbb0d
richardszegh <richard.szegh@gmail.com>
2020-07-01 14:15:01 +0200

    added hero banner

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
-import Layout from '../components/layout/Layout'
+import Layout from '../components/layout'
 import SEO from '../components/utils/SEO'
+
+import HeroBanner from '../components/blocks/HeroBanner'
 
 export default function Home() {
   return (
     <Layout>
       <SEO title="Home" />
 
-      <h1>/index.jsx</h1>
+      <HeroBanner />
     </Layout>
   )
 }
