bc182ece3efc0df944c875f43a8c677d88eda6bd
richardszegh <richard.szegh@gmail.com>
2020-07-02 17:38:08 +0200

    add google font

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,6 +3,12 @@ import Typography from 'typography'
 const typography = new Typography({
   headerFontFamily: ['Nunito', 'sans-serif'],
   bodyFontFamily: ['Nunito', 'serif'],
+  googleFonts: [
+    {
+      name: 'Nunito',
+      styles: ['200', '300', '400', '600', '700'],
+    },
+  ],
 })
 
 export default typography
