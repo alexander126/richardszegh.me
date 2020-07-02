74f07a16267a53cfbc29f9815bb0edf497902827
richardszegh <richard.szegh@gmail.com>
2020-07-02 12:37:29 +0200

    add 404 redirection

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import { navigate } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/utils/SEO'
 
 export default function NotFound() {
+  React.useEffect(() => {
+    navigate('/')
+  }, [])
+
   return (
     <Layout>
       <SEO title="404: Not found" />
-
-      <h1>/404.jsx</h1>
     </Layout>
   )
 }
