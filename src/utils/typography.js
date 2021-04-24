d47c1ee4fae3cad5d241b21c0e3586a2bb89f870
Richárd Szegh <richard.szegh@gmail.com>
2021-04-24 13:56:46 +0200

    ⬆️  upgrade deps & remove typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,14 +1,14 @@
-import Typography from 'typography'
+// import Typography from 'typography'
 
-const typography = new Typography({
-  headerFontFamily: ['Nunito', 'sans-serif'],
-  bodyFontFamily: ['Nunito', 'serif'],
-  googleFonts: [
-    {
-      name: 'Nunito',
-      styles: ['200', '300', '400', '600', '700'],
-    },
-  ],
-})
+// const typography = new Typography({
+//   headerFontFamily: ['Nunito', 'sans-serif'],
+//   bodyFontFamily: ['Nunito', 'serif'],
+//   googleFonts: [
+//     {
+//       name: 'Nunito',
+//       styles: ['200', '300', '400', '600', '700'],
+//     },
+//   ],
+// })
 
-export default typography
+// export default typography
